f79f6d15d4e6058d7022fcb84ba8572a93893d06
Ian Downard <idownard@opnet.com>
2013-05-24 14:09:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyStudy::Application.config.secret_token = '0306a16cd26facf9ac75b7fb71fef22f7691b0459c98fb6c80dc75d9b0ad4abce0741cfdd99b44e8aa63dfe90181e886da3a2739d9b2a00a9c0426be5fa3f693'
